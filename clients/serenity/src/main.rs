d342589630684fc81217b38e19fbe4cda3b95e64
Ludvig Janiuk <ludvig.janiuk@gmail.com>
2016-04-30 21:59:53 +0200

    Added game state struct
    And started using it with adding the botids to it. Now playing this AI
    against itself actually works, earlier it didn't bc of the id issue.

--- clients/serenity/src/main.rs
+++ clients/serenity/src/main.rs
@@ -1,5 +1,4 @@
-extern crate serde;
-extern crate serde_json;
+extern crate serde; extern crate serde_json;
 extern crate websocket;
 extern crate rand;
 
@@ -21,6 +20,7 @@ fn main() {
     println!("Using agent {}", AGENT);
 
     let mut game_on = true;
+    let mut state = State { started: false, bots: Vec::new() };
 
     while game_on {
         let url = Url::parse(format!("ws://{}:{}", GAME_HOST, GAME_PORT).as_ref()).unwrap();
@@ -46,67 +46,13 @@ fn main() {
                 }
             };
 
-            if !handle_message(&mut sender, message) {
+            if !state.handle_message(&mut sender, message) {
                 game_on = false;
                 break;
             }
         }
+        println!("Loop");
     }
-}
-
-fn handle_message<S: Sender>(sender: &mut S, message: Message) -> bool {
-    print!("Got a message... ");
-
-    match message.opcode {
-        Type::Text => {
-            println!("It's text!");
-
-            let pl = from_utf8(&message.payload).unwrap();
-            let message_json: defs::IncomingMessage = serde_json::from_str(&pl).unwrap();
-            match message_json.event_type.as_ref() {
-                "connected" => {
-                    let connected_json: defs::IncomingConnected = serde_json::from_str(&pl).unwrap();
-                    if connected_json.event_type == "connected" {
-                        println!("Got connected message, sending join.");
-                        send_join_message(sender);
-                        println!("Now we wait for start.");
-                    }
-                }
-                "start" => {
-                    // We don't really need to do anything with start?
-                    // We need to get our bot ids, for one. Gotta add that.
-                    println!("Got start message!");
-                }
-                "events" => {
-                    println!("Got som events!");
-                    let event_json: defs::IncomingEvents = serde_json::from_str(&pl).unwrap();
-                    send_actions_message(sender, &event_json);
-
-                }
-                "end" => {
-                    println!("Got end message, we're ending!");
-                    return false;
-                }
-                ev => {
-                    println!("Got unrecognized event type {}, ignoring.", ev);
-                }
-            }
-        }
-        Type::Binary => {
-            println!("It's binary!");
-            let _ = sender.send_message(&Message::binary(message.payload)).unwrap();
-        }
-        Type::Close => {
-            println!("It's a close message, exiting");
-            let _ = sender.send_message(&Message::close());
-            return false;
-        }
-        _ => {
-            println!("Got a weird non-text message from server, ignoring.");
-        }
-    }
-    // Keep playing
-    return true;
 }
 
 fn send_join_message<S: Sender>(sender: &mut S) {
@@ -116,38 +62,108 @@ fn send_join_message<S: Sender>(sender: &mut S) {
     let join_message = Message::text( join_string.to_string() );
     sender.send_message(&join_message).unwrap();
 }
-
-fn send_actions_message<S: Sender>(sender: &mut S, events: &defs::IncomingEvents) {
-    let stupid_actions = defs::ActionsMessage {
-        event_type: "actions".to_string(),
-        round_id: events.round_id,
-        actions: vec![
-            defs::Action {
-                bot_id: 0,
-                action_type: "radar".to_string(),
-                pos: get_random_pos()
-            },
-            defs::Action {
-                bot_id: 1,
-                action_type: "radar".to_string(),
-                pos: get_random_pos()
-            },
-            defs::Action {
-                bot_id: 2,
-                action_type: "radar".to_string(),
-                pos: get_random_pos()
-            }
-        ]
-    };
-    let actions_string = serde_json::to_string(&stupid_actions).unwrap();
-    let actions_message = Message::text( actions_string.to_string() );
-    sender.send_message(&actions_message).unwrap();
-    println!("Sending some random messages");
-}
-
+//
 // Random, doesn't care about the size of the board...
 fn get_random_pos() -> defs::Pos {
     let mut rng = rand::thread_rng();
     defs::Pos { x: rng.gen::<i16>(), y: rng.gen::<i16>() }
 }
+
+struct State {
+    started: bool,
+    bots: Vec<Bot>,
+}
+
+impl State {
+    fn handle_message<S: Sender>(&mut self, sender: &mut S, message: Message) -> bool {
+        print!("Got a message... ");
+
+        match message.opcode {
+            Type::Text => {
+                println!("It's text!");
+
+                let pl = from_utf8(&message.payload).unwrap();
+                let message_json: defs::IncomingMessage = serde_json::from_str(&pl).unwrap();
+
+                match message_json.event_type.as_ref() {
+                    "connected" => {
+                        let connected_json: defs::IncomingConnected = serde_json::from_str(&pl).unwrap();
+                        if connected_json.event_type == "connected" {
+                            println!("Got connected message, sending join.");
+                            send_join_message(sender);
+                            println!("Now we wait for start.");
+                        }
+                    }
+                    "start" => {
+                        println!("Got start message!");
+                        self.started = true;
+                        let start_json: defs::Start = serde_json::from_str(&pl).unwrap();
+                        self.init(&start_json);
+                    }
+                    "events" => {
+                        println!("Got som events!");
+                        let event_json: defs::IncomingEvents = serde_json::from_str(&pl).unwrap();
+                        self.send_actions_message(sender, &event_json);
+
+                    }
+                    "end" => {
+                        println!("Got end message, we're ending!");
+                        return false;
+                    }
+                    ev => {
+                        println!("Got unrecognized event type {}, ignoring.", ev);
+                    }
+                }
+            }
+            _ => {
+                println!("Got a weird non-text message from server, ignoring.");
+            }
+        }
+        // Keep playing
+        return true;
+    }
+
+    fn init(&mut self, start_json: &defs::Start) {
+        for bot_def in &start_json.you.bots {
+            self.bots.push(Bot::new(&bot_def));
+        }
+    }
+
+    fn send_actions_message<S: Sender>(&self, sender: &mut S, events: &defs::IncomingEvents) {
+        let stupid_actions = defs::ActionsMessage {
+            event_type: "actions".to_string(),
+            round_id: events.round_id,
+            actions: self.bots.iter().map(|bot| defs::Action{
+                bot_id: bot.id,
+                action_type: "radar".to_string(),
+                pos: get_random_pos()
+            }).collect(),
+        };
+        let actions_string = serde_json::to_string(&stupid_actions).unwrap();
+        let actions_message = Message::text( actions_string.to_string() );
+        sender.send_message(&actions_message).unwrap();
+        println!("Sending some random messages");
+    }
+}
+
+struct Bot {
+    id: i16,
+    name: String,
+    alive: bool,
+    pos: defs::Pos,
+    hp: i16,
+}
+
+impl Bot {
+    fn new(def: &defs::Bot) -> Bot {
+        return Bot {
+            id: def.bot_id,
+            name: def.name.to_owned(),
+            alive: def.alive,
+            pos: def.pos.unwrap(),
+            hp: def.hp.unwrap(),
+        };
+    }
+}
+
 
