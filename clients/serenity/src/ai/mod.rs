c526654c790487f156ad2721207a02fc7f871e38
Ludvig Janiuk <ludvig.janiuk@gmail.com>
2016-05-04 14:07:27 +0200

    Evade action that spreads bots from each other.
    Also a smart_evade action that chooses between the pread strategy and
    the random (bc if we only have one bot there isn't much spreading to
    do).

--- clients/serenity/src/ai/mod.rs
+++ clients/serenity/src/ai/mod.rs
@@ -38,7 +38,11 @@ impl Ai {
             match *event {
                 Damaged(ref ev) => {
                     println!("Evading on bot {}", ev.bot_id);
-                    actions.push(self.evade_action(self.get_bot(ev.bot_id).unwrap()));
+                    actions.push(self.evade_spread(self.get_bot(ev.bot_id).unwrap()));
+                }
+                Detected(ref ev) => {
+                    println!("Evading on bot {}", ev.bot_id);
+                    actions.push(self.evade_spread(self.get_bot(ev.bot_id).unwrap()));
                 }
                 Echo(ref ev) => {
                     println!("Got echo, gonna shoot at it!");
@@ -72,6 +76,14 @@ impl Ai {
     }
 
     fn evade_action(&self, bot: &Bot) -> Action {
+        if self.botsAlive() >= 2 {
+            return self.evade_spread();
+        } else {
+            return self.evade_random();
+        }
+    }
+
+    fn evade_random(&self, bot: &Bot) -> Action {
         let neighbors = bot.pos.neighbors(&self.config.moves_allowed); 
         let move_to = *rand::thread_rng().choose(&neighbors).expect("Oh there were no neighbors? That's impossible.");
         println!("MOVES: {}, {}, {}, {}", bot.pos.x, bot.pos.y, move_to.x, move_to.y);
@@ -84,19 +96,22 @@ impl Ai {
 
     fn evade_spread(&self, bot: &Bot) -> Action {
         let neighbors = bot.pos.neighbors(&self.config.moves_allowed); 
-        let otherbots = self.bots.iter()
+        let otherbots: Vec<&Bot> = self.bots.iter()
             .by_ref()
             .filter(|otherbot| otherbot.id != bot.id)
             .collect();
             
-        let move_to = neighbors.iter().max_by_key(
-            |pos| otherbots.iter().min_by_key(
-                |otherbot| pos.distance(otherbot.pos)));
+        let move_to: &Pos = neighbors.iter()
+            .max_by_key( |pos| otherbots.iter()
+                .map( |otherbot| pos.distance(otherbot.pos))
+                .min()
+                .expect("There should be other bots"))
+            .expect("There should be neighbor positions");
 
         return Action {
             bot_id: bot.id,
             action_type: MOVE.to_string(),
-            pos: move_to,
+            pos: move_to.clone(),
         };
     }
 
