48ed22ca715a8860be5b71cbfa875ef1d70f9b50
Fredrik Andersson <fredrik.andersson@widespace.com>
2016-05-05 11:14:19 +0200

    ActionList added to root

---
+++ clients/serenity/src/lists/mod.rs
@@ -0,0 +1,40 @@
+use ai::*;
+use util;
+use defs::{Action};
+use position::Pos;
+use strings::{ RADAR };
+
+
+pub trait ActionsList {
+    // Naming?
+    fn populate(ai: &Ai) -> Vec<Action>;
+    fn get_action_mut(&mut self, id: i16) -> Option<&mut Action>;
+    fn set_action_for(&mut self, id: i16, action: &str, pos: Pos);
+}
+
+impl ActionsList for Vec<Action> {
+    // Populate a default action for each bot with random radar
+    fn populate(ai: &Ai) -> Vec<Action> {
+        ai.bots
+            .iter()
+            .map(|b| Action {
+                bot_id: b.id,
+                action_type: RADAR.to_string(),
+                pos: util::get_random_pos(&ai.radar_positions)
+            })
+            .collect::<Vec<Action>>()
+    }
+
+    fn get_action_mut(&mut self, id: i16) -> Option<&mut Action> {
+        self
+            .iter_mut()
+            .find(|ac|ac.bot_id == id)
+    }
+
+    fn set_action_for(&mut self, id: i16, action_type: &str, pos: Pos) {
+        if let Some(action) = self.get_action_mut(id) {
+            action.action_type = action_type.to_string();
+            action.pos = pos;
+        }
+    }
+}
