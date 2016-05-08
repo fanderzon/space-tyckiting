ab6949ed44308affe9ad4d58de6a0032c74f3065
Ludvig Janiuk <ludvig.janiuk@gmail.com>
2016-05-08 14:03:32 +0200

    A better triangle shape
    After doing some math, I noticed that our current traingle shape was
    pretty bad: It was shooting at six places where the ship could
    impossibly be after one move, while missing the middle, where the ship
    could very well be.
    
    So I tightened the triangle and now we get three shots at the middle, no
    completely wasted ones, and three missed potential positions (but that
    was already the case before too).
    
    I just noticed however that the previous pattern had potential for more
    wide-sweeping shooting (not one move after detecting enemy.

--- clients/serenity/src/position/mod.rs
+++ clients/serenity/src/position/mod.rs
@@ -11,8 +11,8 @@ include!(concat!(env!("OUT_DIR"), "/position.rs"));
 impl Pos {
     pub fn triangle_smart(&self) -> Vec<Pos> {
         let mut triangle = match rand::thread_rng().gen_range(0, 2) {
-            0 => { self.triangle_down() }
-            _ => { self.triangle_up() }
+            0 => { self.triangle_left() }
+            _ => { self.triangle_right() }
         };
         // Shuffle so that the same will not be middled every time
         let mut rng = rand::thread_rng();
@@ -28,20 +28,20 @@ impl Pos {
     }
 
     // TODO: Generalize for shot radius
-    pub fn triangle_down(&self) -> Vec<Pos> {
+    fn triangle_left(&self) -> Vec<Pos> {
         let x = self.x;
         let y = self.y;
-        return vec![ Pos::new(x-1, y+2),
-                     Pos::new(x+2, y-1),
-                     Pos::new(x-1, y-1) ];
+        return vec![ Pos::new(x-1, y  ),
+                     Pos::new(x+1, y-1),
+                     Pos::new(x,   y+1) ];
     }
 
     // TODO: Generalize for shot radius
-    pub fn triangle_up(&self) -> Vec<Pos> {
+    pub fn triangle_right(&self) -> Vec<Pos> {
         let x = self.x;
         let y = self.y;
-        return vec![ Pos::new(x+1, y-2),
-                     Pos::new(x-2, y+1),
-                     Pos::new(x+1, y+1) ];
+        return vec![ Pos::new(x+1, y  ),
+                     Pos::new(x-1, y+1),
+                     Pos::new(x,   y-1) ];
     }
 }
