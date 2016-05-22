d225869749c71dec3a01524a5f05de788249977b
Fredrik Andersson <fredrik.andersson@widespace.com>
2016-05-22 11:02:19 +0200

    Setup done for historic echo search, echoes are grouped by round_id and looped in reverse order.

--- clients/serenity/src/ai/scan.rs
+++ clients/serenity/src/ai/scan.rs
@@ -1,8 +1,10 @@
 use defs::Action;
+use position::Pos;
 use strings::{RADAR, NOACTION};
 use ai::*;
 use util;
-use lists::ActionsList;
+use lists::{ ActionsList, HistoryList };
+use lists::ActionMode::*;
 
 impl Ai {
     // Might be good to keep around as an option?
@@ -16,7 +18,7 @@ impl Ai {
     }
 
     pub fn scan_with_idle_bots(&mut self, actions: &mut Vec<Action>) {
-        let (ref mut radar_index, ref positions) = self.radar_positions;
+        // Get the bots available for scanning
         let idle_bots: Vec<i16> = self.bots
             .iter()
             .cloned()
@@ -31,16 +33,46 @@ impl Ai {
             .collect();
 
 
-        for bot_id in idle_bots {
-            // replace with better radar logic
-            if *radar_index > positions.len() as i16 - 1 {
-                *radar_index = 0;
-            }
-            let target = self.radar_positions.1[*radar_index as usize];
-            actions.set_action_for(bot_id, RADAR, target);
-            *radar_index += 1;
+        // Let's check if we were attacking last round
+        match self.history.get_mode(self.round_id - 1) {
+            // If so, and we switched back to scanning, let's see if we can find an echo we missed previously
+            Attack => {
 
-            self.logger.log(&format!("Scanning with Bot {} on {} b/c it was idle.", bot_id, target), 2);
+                let echo_positions: Vec<(i16,Vec<Pos>)> = self.history.get_echo_positions(30)
+                    .iter()
+                    .fold(vec![], |mut acc, curr| {
+                        let &(pos, round_id) = curr;
+                        if acc.iter().find(|tup| tup.0 == round_id).is_some() {
+                            if let Some(tup) = acc.iter_mut().find(|tup| tup.0 == round_id) {
+                                tup.1.push(pos);
+                            }
+                        } else {
+                            acc.push( (round_id, vec![pos]) );
+                        }
+                        acc
+                    });
+
+
+                for (ref round_id, ref positions) in echo_positions.iter().rev().cloned().collect::<Vec<_>>() {
+                    println!("round_id: {}: {:?}", round_id, positions);
+
+                }
+            },
+            _ => {
+                // Resume basic sequential scanning
+                let (ref mut radar_index, ref positions) = self.radar_positions;
+                for bot_id in idle_bots {
+                    // replace with better radar logic
+                    if *radar_index > positions.len() as i16 - 1 {
+                        *radar_index = 0;
+                    }
+                    let target = self.radar_positions.1[*radar_index as usize];
+                    actions.set_action_for(bot_id, RADAR, target);
+                    *radar_index += 1;
+
+                    self.logger.log(&format!("Scanning with Bot {} on {} b/c it was idle.", bot_id, target), 2);
+                }
+            },
         }
     }
 }
