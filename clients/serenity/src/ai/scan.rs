fe7a6a5a3059a29c3e0f3dcaabb5c1cd4fda3bbc
Fredrik Andersson <fredrik.andersson@widespace.com>
2016-05-22 15:16:32 +0200

    Let's not try to get the last rounds mode if there is no last round...

--- clients/serenity/src/ai/scan.rs
+++ clients/serenity/src/ai/scan.rs
@@ -37,70 +37,72 @@ impl Ai {
         let mut unexplored_echo = Pos::new(-1,-1);
 
         // Let's check if we were attacking last round
-        match self.history.get_mode(self.round_id - 1) {
-            // If so, and we switched back to scanning, let's see if we can find an echo we missed previously
-            Attack => {
-                // Group the echo positions by round_id
-                let echo_positions: Vec<(i16,Vec<Pos>)> = self.history.get_echo_positions(50)
-                    .iter()
-                    // Filter out known asteroid positions
-                    .filter(|tup| {
-                        !self.is_pos_a_recorded_asteroid(&tup.0)
-                    })
-                    .fold(vec![], |mut acc, curr| {
-                        let &(pos, round_id) = curr;
-                        if acc.iter().find(|tup| tup.0 == round_id).is_some() {
-                            if let Some(tup) = acc.iter_mut().find(|tup| tup.0 == round_id) {
-                                tup.1.push(pos);
+        if self.round_id > 0 {
+            match self.history.get_mode(self.round_id - 1) {
+                // If so, and we switched back to scanning, let's see if we can find an echo we missed previously
+                Attack => {
+                    // Group the echo positions by round_id
+                    let echo_positions: Vec<(i16,Vec<Pos>)> = self.history.get_echo_positions(50)
+                        .iter()
+                        // Filter out known asteroid positions
+                        .filter(|tup| {
+                            !self.is_pos_a_recorded_asteroid(&tup.0)
+                        })
+                        .fold(vec![], |mut acc, curr| {
+                            let &(pos, round_id) = curr;
+                            if acc.iter().find(|tup| tup.0 == round_id).is_some() {
+                                if let Some(tup) = acc.iter_mut().find(|tup| tup.0 == round_id) {
+                                    tup.1.push(pos);
+                                }
+                            } else {
+                                acc.push( (round_id, vec![pos]) );
                             }
-                        } else {
-                            acc.push( (round_id, vec![pos]) );
-                        }
-                        acc
-                    });
+                            acc
+                        });
 
-                for (ref round_id, ref positions) in echo_positions.iter().rev().cloned().collect::<Vec<_>>() {
-                    // If we have more than one echo, find out which one we didn't pursue
-                    if positions.len() > 1 {
-                        let cannon_actions = self.history.get_actions_for_round( CANNON, round_id + 1);
-                        let radar_actions = self.history.get_actions_for_round( RADAR, round_id + 1);
+                    for (ref round_id, ref positions) in echo_positions.iter().rev().cloned().collect::<Vec<_>>() {
+                        // If we have more than one echo, find out which one we didn't pursue
+                        if positions.len() > 1 {
+                            let cannon_actions = self.history.get_actions_for_round( CANNON, round_id + 1);
+                            let radar_actions = self.history.get_actions_for_round( RADAR, round_id + 1);
 
-                        // if there is a radar action it tells us exactly which position we went for
-                        let mut targeted_echo = Pos::new(-1,-1);
-                        if radar_actions.len() > 1 {
-                            targeted_echo = radar_actions[0].pos;
-                        } else if cannon_actions.len() > 0 {
-                            let mut considered = cannon_actions[0].pos.neighbors(4);
-                            considered.push(cannon_actions[0].pos);
+                            // if there is a radar action it tells us exactly which position we went for
+                            let mut targeted_echo = Pos::new(-1,-1);
+                            if radar_actions.len() > 1 {
+                                targeted_echo = radar_actions[0].pos;
+                            } else if cannon_actions.len() > 0 {
+                                let mut considered = cannon_actions[0].pos.neighbors(4);
+                                considered.push(cannon_actions[0].pos);
 
-                            let action_tup = considered
-                                .iter()
-                                .fold( (1000,Pos::new(0,0) ), |acc, pos| {
-                                    // Total distance of this position from all cannons positions
-                                    let d: i16 = cannon_actions
-                                        .iter()
-                                        .fold(0, |acc,curr| acc + curr.pos.distance(*pos));
-                                    // Accumulate the position with the lowest ditance
-                                    if d < acc.0 {
-                                        (d, *pos)
-                                    }  else {
-                                        acc
-                                    }
-                                });
-                            targeted_echo = action_tup.1;
-                        }
-
-                        if targeted_echo.x >= 0 {
-                            if let Some(ac) = cannon_actions.iter().find(|ac| ac.pos != targeted_echo) {
-                                unexplored_echo = ac.pos;
+                                let action_tup = considered
+                                    .iter()
+                                    .fold( (1000,Pos::new(0,0) ), |acc, pos| {
+                                        // Total distance of this position from all cannons positions
+                                        let d: i16 = cannon_actions
+                                            .iter()
+                                            .fold(0, |acc,curr| acc + curr.pos.distance(*pos));
+                                        // Accumulate the position with the lowest ditance
+                                        if d < acc.0 {
+                                            (d, *pos)
+                                        }  else {
+                                            acc
+                                        }
+                                    });
+                                targeted_echo = action_tup.1;
                             }
+
+                            if targeted_echo.x >= 0 {
+                                if let Some(ac) = cannon_actions.iter().find(|ac| ac.pos != targeted_echo) {
+                                    unexplored_echo = ac.pos;
+                                }
+                            }
+
+
                         }
-
-
                     }
-                }
-            },
-            _ => (),
+                },
+                _ => (),
+            }    
         }
 
         // Do we have a lead of where to scan?
